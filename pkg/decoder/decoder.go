aa8f4f6eae1912c85cdbd4925bd38183d1fcd78d
Duncan McGreggor <oubiwann@gmail.com>
2022-11-28 16:23:39 -0600

    Let's see about accepting even non-dist headers.

--- pkg/decoder/decoder.go
+++ pkg/decoder/decoder.go
@@ -13,8 +13,10 @@ func Decode(data []byte) (interface{}, error) {
 	// We're expecting the dist header; see:
 	// * http://erlang.org/doc/apps/erts/erl_ext_dist.html#distribution_header
 	if data[0] != distHeaderByte {
-		log.Error(ErrUnexpectedHeader)
-		return nil, ErrUnexpectedHeader
+		// log.Error(ErrUnexpectedHeader)
+		// return nil, ErrUnexpectedHeader
+		log.Warn("data not in distribution format ... attempting conversion")
+		data = ToDist(data)
 	}
 	// Once confirmed, let's remove it and parse the binary-encoded terms:
 	term, _, err := etf.Decode(data[1:], []etf.Atom{}, etf.DecodeOptions{})
