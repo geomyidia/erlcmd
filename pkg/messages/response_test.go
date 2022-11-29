b1027bb520a5762945635e0d6816744fd6586c0d
Duncan McGreggor <oubiwann@gmail.com>
2022-11-28 20:28:48 -0600

    Don't need it in the test anymore.

--- pkg/messages/response_test.go
+++ pkg/messages/response_test.go
@@ -7,7 +7,6 @@ import (
 	"github.com/stretchr/testify/suite"
 
 	"github.com/geomyidia/erlcmd/pkg/decoder"
-	"github.com/geomyidia/erlcmd/pkg/util"
 )
 
 type ResponseTestSuite struct {
@@ -22,7 +21,7 @@ func (s *ResponseTestSuite) TestBytes() {
 	s.Require().NoError(err)
 	bytes, err := r.Bytes()
 	s.Require().NoError(err)
-	parsed, err := decoder.Decode(util.ToDist(bytes))
+	parsed, err := decoder.Decode(bytes)
 	s.Require().NoError(err)
 	s.Equal(etf.Tuple{etf.Atom("result"), etf.Atom("ok")}, parsed)
 }
